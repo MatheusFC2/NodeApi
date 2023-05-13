a6cc5d97df13aa91a42969599ba4f5209f8cd5d1
MatheusFC2 <matheusfreitascampos@gmail.com>
2023-05-13 17:06:00 -0300

    show list users

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import UserController from "./controller/UserController";
 
 const routes = Router();
 
+routes.get("/users", UserController.find);
 routes.post("/user", UserController.create)
 
-export default routes;
+export default routes;  
