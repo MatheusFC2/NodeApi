b404784e308373f05079061d9cde50db6331d28c
MatheusFC2 <matheusfreitascampos@gmail.com>
2023-05-12 22:00:40 -0300

    installed mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("Olassss")
+import express from 'express'; 
+
+const app = express();
+
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("ola");
+})
+
+
+app.listen(3000, () => {
+    console.log('listening on port 3000🚀');
+});
